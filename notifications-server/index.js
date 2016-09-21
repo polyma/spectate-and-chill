b198759b5fc958a4429ac918b7d65640f889e2ba
Polyma <polyma@syner.gg>
2016-09-21 15:47:01 -0700

    added redis

--- notifications-server/index.js
+++ notifications-server/index.js
@@ -4,9 +4,13 @@ var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  redis.get('hi', function(err, result) {
+    res.send('<h1>' + result + '</h1>');
+  });
 });
 
 http.listen(process.env.PORT, function(){
   console.log('listening on *:' + process.env.PORT);
 });
+
+redis.set('hi', 'test');
