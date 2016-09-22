0b697d8700e0c3301df54fe30267c7e91448a9ba
Rob <robrua@alumni.cmu.edu>
2016-09-21 20:36:30 -0700

    don't autoretry

--- spectate_and_chill/spectate_and_chill/urls.py
+++ spectate_and_chill/spectate_and_chill/urls.py
@@ -16,42 +16,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from cassiopeia import riotapi, baseriotapi
-from cassiopeia.type.api import APIError
-
-
-# Initialize cassiopeia settings
-def auto_retry(api_call_method):
-    """ A decorator to automatically retry 500s (Service Unavailable) and skip 400s (Bad Request) or 404s (Not Found). """
-    def call_wrapper(*args, **kwargs):
-        try:
-            return api_call_method(*args, **kwargs)
-        except APIError as error:
-            # Try Again Once
-            if error.error_code in [500]:
-                try:
-                    return api_call_method(*args, **kwargs)
-                except APIError as another_error:
-                    if another_error.error_code in [500, 400, 404]:
-                        return None
-                    else:
-                        raise another_error
-
-            # Skip
-            elif error.error_code in [400, 404]:
-                return None
-
-            # Fatal
-            else:
-                raise error
-    return call_wrapper
+from cassiopeia import riotapi
 
 
 riotapi.set_load_policy("lazy")
 riotapi.set_rate_limit((25000, 10))
 riotapi.set_data_store(None)
 riotapi.set_api_key("RGAPI-e4491f0b-b99a-49c4-b817-5f9b00267da1")
-baseriotapi.get_current_game = auto_retry(baseriotapi.get_current_game)
 
 
 urlpatterns = [
