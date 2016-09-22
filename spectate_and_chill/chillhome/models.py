aa222d43114143ff057622566df15d8fe4e7b3a1
Hackathon <Hackathon>
2016-09-21 19:09:26 -0700

    Fixed Models

--- spectate_and_chill/chillhome/models.py
+++ spectate_and_chill/chillhome/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 # Create your models here.
 
 class MatchEverything(models.Model):
-    matchId = Models.IntergerField() #Long?
-    region = Models.CharField()
-    json = Models.JsonField() # Double Check
+    matchId = models.IntegerField() #Long?
+    region = models.CharField()
+    json = JSONField() # Double Check
     
     # Primary key is the matchId + region 
     
