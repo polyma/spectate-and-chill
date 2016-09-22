1e0b2b2643747e6204ed92b4bdbcb922118d9b77
Polyma <polyma@syner.gg>
2016-09-21 20:47:30 -0700

    reds working!

--- notifications-server/index.js
+++ notifications-server/index.js
@@ -1,16 +1,19 @@
 var Redis = require('ioredis');
-var redis = new Redis(process.env.REDIS_URI);
+var sub = new Redis(process.env.REDIS_URI);
+sub.on('connect', function(err, count) {
+  console.log('connected to redis');
+  sub.subscribe('event');
+});
+sub.on('message', function(channel, message) {
+  console.log('message', message, 'received from', channel);
+});
 var app = require('express')();
 var http = require('http').Server(app);
 
 app.get('/', function(req, res){
-  redis.get('hi', function(err, result) {
-    res.send('<h1>' + result + '</h1>');
-  });
+    res.send('<h1>HI</h1>');
 });
 
 http.listen(process.env.PORT, function(){
   console.log('listening on *:' + process.env.PORT);
 });
-
-redis.set('hi', 'test');
--- spectate_and_chill/chillhome/views.py
+++ spectate_and_chill/chillhome/views.py
@@ -3,23 +3,23 @@ from django.http import HttpResponse, Http404
 
 import json
 import time
-import redis 
+import redis
 
 
 # Create your views here.
 def index(request):
     #return HttpResponse("You made it!")
     return render(request, 'chillhome/index.html')
-    
-    
+
+
 def request_summoner(request):
     return HttpResponse()
-    
+
 def delay404(request):
     time.sleep(5)
     raise Http404()
 
 def redisTest(request):
-    r = redis.Redis(host="localhost", port=6379)
+    r = redis.Redis(host="redis", port=6379)
     r.publish("event", "hello world")
-    
+    return HttpResponse()
