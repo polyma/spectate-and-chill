3e6cf4b6c4973d20cbabeb27217b2c3a9a553231
Polyma <polyma@syner.gg>
2016-09-22 20:24:24 -0700

    lots of updating

--- spectate_and_chill/chillhome/views.py
+++ spectate_and_chill/chillhome/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, Http404
 
 import urllib.request
 import urllib.error
-import urllib.parse 
+import urllib.parse
 import json
 import time
 import redis
@@ -32,6 +32,7 @@ def request_summoner(request):
         # Request the API
         region = (request.GET.get("region")).lower()
         summonerName = request.GET.get("summonerName")
+        print(region, summonerName)
 
         #baseriotapi.set_region(region)
         #summoner = riotapi.get_summoner_by_name(summonerName)
@@ -43,59 +44,54 @@ def request_summoner(request):
         )
         r = urllib.request.Request(url)
         response = urllib.request.urlopen(r)
-        
+
         j = json.loads(response.read().decode('utf-8'))
-        
+
         summonerJson = j[list(j)[0]]
-        
-        summoner, created = User.objects.update_or_create(
-            summonerId = summonerJson["id"],
-            region = region,
-            default = {
-                "summonerName":summonerJson["name"],
-                "summonerIcon":summonerJson["profileIconId"],
-                "summonerSimpleName":summonerName,
-            }
-        )
+
+        # summoner, created = User.objects.update_or_create(
+        #     summonerId = summonerJson["id"],
+        #     region = region,
+        #     default = {
+        #         "summonerName":summonerJson["name"],
+        #         "summonerIcon":summonerJson["profileIconId"],
+        #         "summonerSimpleName":summonerName,
+        #     }
+        # )
 
     except:
         # Something was wrong or went wrong, assume the summoner doesn't exist
         raise Http404('Invalid Summoner')
-        
+
     try:
         if created:
             # New user, new recommendations
             pass
-            
-        
-    
-        #dummyData = [{
-        #        "id": "streamer",
-        #        "displayName":"MushIsGosu",
-        #        "name":"muchisgosu",
-        #        "language":"en",
-        #        "logo":"https://static-cdn.jtvnw.net/jtv_user_pictures/mushisgosu-profile_image-b1c8bb5fd700025e-300x300.png",
-        #        "status":"TSM Gosu - Solo Q - Shadowverse later",
-        #        "currentViews":7333,
-        #        "totalViews":78560127,
-        #        "followers":1096293,
-        #
-        #        "spectateURL":"<complete gibberish>",
-        #        "twitchURL":"https://www.twitch.tv/mushisgosu",
-        #        "previewURL_small":"https://static-cdn.jtvnw.net/previews-ttv/live_user_mushisgosu-80x45.jpg",
-        #        "previewURL_medium":"https://static-cdn.jtvnw.net/previews-ttv/live_user_mushisgosu-320x180.jpg",
-        #        "previewURL_large":"https://static-cdn.jtvnw.net/previews-ttv/live_user_mushisgosu-640x360.jpg",
-        #        "championId":67,
-        #        "lane":"",
-        #        "followedBy":[{
-        #            "id": "%s_%s"%(summoner.id, region),
-        #            "summonerId":summoner.id,
-        #            "region":region,
-        #        }]
-        #}]
-        #r = redis.Redis(host=redisServer, port=6379)
-        #r.publish("event", json.dumps(dummyData))
-        #return HttpResponse()
+
+
+
+        dummyData = [{
+               "id": "streamer",
+               "displayName":"MushIsGosu",
+               "name":"muchisgosu",
+               "language":"en",
+               "logo":"https://static-cdn.jtvnw.net/jtv_user_pictures/mushisgosu-profile_image-b1c8bb5fd700025e-300x300.png",
+               "status":"TSM Gosu - Solo Q - Shadowverse later",
+               "currentViews":7333,
+               "totalViews":78560127,
+               "followers":1096293,
+
+               "spectateURL":"<complete gibberish>",
+               "twitchURL":"https://www.twitch.tv/mushisgosu",
+               "previewURL_small":"https://static-cdn.jtvnw.net/previews-ttv/live_user_mushisgosu-80x45.jpg",
+               "previewURL_medium":"https://static-cdn.jtvnw.net/previews-ttv/live_user_mushisgosu-320x180.jpg",
+               "previewURL_large":"https://static-cdn.jtvnw.net/previews-ttv/live_user_mushisgosu-640x360.jpg",
+               "championId":67,
+               "lane":"",
+        }]
+        # r = redis.Redis(host=redisServer, port=6379)
+        # r.publish("event", json.dumps(dummyData))
+        return HttpResponse(json.dumps(dummyData))
     except Exception as e:
         raise Http404('KABOOM \n%s'%e)
 
@@ -117,28 +113,28 @@ def recommendations(request):
         summonerName = request.GET.get("summonerName")
     except:
         raise Http404("Must provide region and summonerName")
-        
+
     user = None
     try:
         user = User.objects.get(region=region, summonerSimpleName=summonerName)
     except:
         raise Http404("No such summoner in DB")
-        
+
 
     # Return recommendations
     recommendations = Recommendation.objects.filter(User=user)
-    
+
     content = []
     for recommended in recommendations:
         info = {}
         info["score"] = recommended.score
         info["streamerName"] = recommended.streamer.streamName
         content.append(info)
-        
-    
+
+
     return HttpResponse(json.dumps(content))
 
-        
+
     #dummyData = [
     #        {
     #            "summonerName":"Hi Im Gosu",
@@ -154,4 +150,3 @@ def recommendations(request):
     #        }
     #]
     #return HttpResponse(json.dumps(dummyData))
-    
