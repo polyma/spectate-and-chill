cfe6a367540dddcf77905c4e21c8cfd5543c38ec
Polyma <polyma@syner.gg>
2016-09-22 19:18:15 -0700

    FIXING THESE THINGS FFS

--- spectate_and_chill/chillhome/assets/js/actions/SocketActionCreators.js
+++ spectate_and_chill/chillhome/assets/js/actions/SocketActionCreators.js
@@ -1,5 +1,6 @@
 const ServerConstants = require('../constants/ServerConstants');
 const StreamConstants = require('../constants/StreamConstants');
+import fetch from 'isomorphic-fetch'
 
 
 export function newSocket(socket) {
@@ -26,5 +27,28 @@ export function receiveStreams(streams) {
 export function disconnect() {
   return {
     type: ServerConstants.ActionTypes.DISCONNECT_SERVER,
+  }
+}
+
+export function getRecommendations() {
+  return {
+    type: StreamConstants.ActionTypes.REQUEST_RECOMMENDATIONS,
+  }
+}
+
+export function getRecommendations(userId) {
+  return function (dispatch) {
+    dispatch(getRecommendations());
+    return fetch(window.serverUrl + '/recommendations?user_id=' + userId)
+      .then(response => response.json())
+      .then(json => {
+        // We can dispatch many times!
+        // Here, we update the app state with the results of the API call.
+        console.log('received result for recommendations', json);
+        dispatch(receiveStreams(json));
+      })
+//NOTE: WE MAY NEED import 'babel-polyfill' http://redux.js.org/docs/advanced/AsyncActions.html
+      // In a real world app, you also want to
+      // catch any error in the network call.
   }
 }
